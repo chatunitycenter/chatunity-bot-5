f9b04be70bbaa596f84d94b214c3a0db390d77a2
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-06-04 22:44:26 +0200

    Update menu-principale.js

--- plugins/menu-principale.js
+++ plugins/menu-principale.js
@@ -24,7 +24,7 @@ const handler = async (message, { conn, usedPrefix }) => {
     
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    const videoPath = path.join(__dirname, '../menu/chatunity-bot.gif'); 
+    const videoPath = path.join(__dirname, '../menu/chatunity-bot.jpg'); 
 
     
     await conn.sendMessage(message.chat, {
